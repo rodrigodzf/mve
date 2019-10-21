043702ca2fbac3b918b3aea6794ed1b0af82ddfa
rodrigodzf <rodrigodzf@gmail.com>
2019-10-21 12:11:34 +0200

    Use glew.h always

--- libs/ogl/opengl.h
+++ libs/ogl/opengl.h
@@ -17,17 +17,6 @@
 #ifndef OGL_OPEN_GL_HEADER
 #define OGL_OPEN_GL_HEADER
 
-#if defined(OGL_USE_OSMESA)
-#  define GL_GLEXT_PROTOTYPES
-#  include <GL/osmesa.h>
-#elif defined(__APPLE__)
-#  include <OpenGL/gl3.h>
-#elif defined(_WIN32)
-#  include <GL/glew.h>
-#else
-#  define GL_GLEXT_PROTOTYPES
-#  include <GL/gl.h>
-#  include <GL/glext.h>
-#endif
+#include <GL/glew.h>
 
 #endif /* OGL_OPEN_GL_HEADER */
